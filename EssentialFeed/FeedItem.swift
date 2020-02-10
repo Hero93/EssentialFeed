2eb6db87813c1f528078d645e6388c7b9c5a5aa1
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-10 19:02:52 +0100

    create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Luca LG. Gramaglia on 10/02/2020.
+//  Copyright © 2020 lucagramaglia. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
