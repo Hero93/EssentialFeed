2cc6ceb67b16530ad4f2e2277ee38c9530d9dd33
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-15 16:57:37 +0100

    added comments about the design choices

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,14 +8,28 @@
 
 import Foundation
 
+// There's no reason to make the HTTPClient a singleton, apart from convenience to locate the instace directly.
+// It doesn't need to be a class either, it is just a contract defining which external functionality the "RemoteFeedLoader" needs.
+
+// We don't need to create a new type to conform to it, we can easily create an extension on URLSession or Alamofire.
+
 public protocol HTTPClient {
     func get(from url: URL)
 }
+
+// "RemoteFeedLoader" will not depend on concrete type like URLSession but by creating a clean separation between protocols,
+// we make the RemoteFeedLoader more flexible, open for extensions and more testable.
+
 
 public class RemoteFeedLoader {
     
     let client: HTTPClient
     let url: URL
+    
+    // The "RemoteFeedLoader" doesn't need to locate or instantiate the HTTPClient instance.
+    // We can make our code more modular, by injecting the HTTPClient as a dependency. (Open/Close principle)
+    
+    // Usually the HTTPClient is created as a Singleton -> by doing this I introduce high coupoling between the modules.
     
     public init(url: URL, client: HTTPClient) {
         self.client = client
