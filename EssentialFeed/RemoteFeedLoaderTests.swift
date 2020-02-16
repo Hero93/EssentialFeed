d78bc477ccc4a10e1ffa4e3cfbb60dc5887e8e80
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-16 16:55:37 +0100

    Add more test assertions for diverse HTTP status code

--- EssentialFeed/RemoteFeedLoaderTests.swift
+++ EssentialFeed/RemoteFeedLoaderTests.swift
@@ -78,14 +78,17 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
 
         // Act
-        
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load() { capturedErrors.append($0) }
-        
-        client.complete(withStatusCode: 400)
-        
-        // Assert
-        XCTAssertEqual(capturedErrors, [.inavalidData])
+        var samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load() { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            // Assert
+            XCTAssertEqual(capturedErrors, [.inavalidData])
+        }
     }
     
     // MARK: Helpers
