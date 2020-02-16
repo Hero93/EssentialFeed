4642468f88f3a03b618f5ad597c71652b96e068f
Luca LG. Gramaglia <luca.gramaglia@e-gate.it>
2020-02-16 15:35:07 +0100

    Replace stubbing by capturing value instead

--- EssentialFeed/RemoteFeedLoaderTests.swift
+++ EssentialFeed/RemoteFeedLoaderTests.swift
@@ -58,18 +58,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_load_deliverErrorOnClientError() {
+        // Arrange
         let (sut, client) = makeSUT()
-        // We are mixing concepts: stubbing an capturing values.
-        // Here we stub the client even though is a spy and before call "load" -> that doesn't
-        // reflect the async nature of this behaviour.
-        
-        // we want to keep the spy a spy: just capturing data instead of stubbing it with some behaviour
-        
-        client.error = NSError(domain: "Test", code: 0)
+
+        // Act
         
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load() { capturedErrors.append($0) }
         
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
+        
+        // Assert
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -83,12 +83,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        // Will hold of the completions blocks passed
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
