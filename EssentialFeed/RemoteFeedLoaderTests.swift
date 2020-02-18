6ba43ba46124e353c27898d0aec392da15415fa9
Luca Gramaglia <lucagramaglia@icloud.com>
2020-02-18 22:02:27 +0100

    Create factory methods to simplify tests setup

--- EssentialFeed/RemoteFeedLoaderTests.swift
+++ EssentialFeed/RemoteFeedLoaderTests.swift
@@ -101,32 +101,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
         let (sut, client) = makeSUT()
         
-        let item1 = FeedItem(id: UUID(),
-                             description: nil,
-                             location: nil,
+        let item1 = makeItem(id: UUID(),
                              imageURL: URL(string: "http://a-url.com")!)
-        let item1JSON = [
-            "id": item1.id.uuidString,
-            "image": item1.imageURL.absoluteString
-        ]
         
-        let item2 = FeedItem(id: UUID(),
+        let item2 = makeItem(id: UUID(),
                              description: "A description",
                              location: "A Location",
                              imageURL: URL(string: "http://a-url.com")!)
-        let item2JSON = [
-            "id": item2.id.uuidString,
-            "description": item2.description,
-            "location": item2.location,
-            "image": item2.imageURL.absoluteString
-        ]
         
-        let itemsJSON = [
-            "items": [item1JSON, item2JSON]
-        ]
-        
-        expect(sut, toCompleteWithResult: .success([item1, item2]), when: {
-            let json = try! JSONSerialization.data(withJSONObject: itemsJSON)
+        let items = [item1.model, item2.model]
+                
+        expect(sut, toCompleteWithResult: .success(items), when: {
+            let json = makeItemsJSON([item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json)
         })
     }
@@ -152,6 +138,28 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         // When the test fails, it fails in the exact line, not here.
         XCTAssertEqual(capturedResults, [result], file: file, line: line)
+    }
+    
+    // Factory method
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+        let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
+
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].reduce(into: [String: Any]()) { (acc, e) in
+            // this functions adds only the non-nil values inside the dictionary
+            if let value = e.value { acc[e.key] = value }
+        }
+
+        return (item, json)
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     // The spy job is to capture the messages (invokations) in a clear way.
