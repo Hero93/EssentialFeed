21cbbbb79a55f0da3ba412a131eec7a8b875ac72
Luca Gramaglia <lucagramaglia@icloud.com>
2020-03-08 14:46:28 +0100

    Resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -17,11 +17,15 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
+    
+    // This 2 test check exacly the implementation of URLSession class
+    // but we don't want that.
+    // We don't own this class so it's better to check the behaviour not the implementation
     
     func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
@@ -29,8 +33,23 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient(session: session)
         
         sut.get(from: url)
-                
+        
         XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        // Now we need to tell the session to return our task for a given url.
+        // We create a stub method.
+        session.stub(url: url, with: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
     }
     
     // MARK: - Helpers
@@ -43,18 +62,34 @@ class URLSessionHTTPClientTests: XCTestCase {
     private class URLSessionSpy: URLSession {
         
         // We want to check that "URLSession" class receives the correct url
-        // We don't have access to the received urls so we need to "spy" the class
+        // but We don't have access to the received urls so we need to "spy" the class.
         var receivedURLs = [URL]()
+        
+        private var stubs = [URL: URLSessionDataTask]()
         
         override init() {}
         
+        func stub(url: URL, with task: URLSessionDataTask) {
+            stubs[url] = task
+        }
+        
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
     
     private class FakeURLSessionDataTask: URLSessionDataTask {
         override init() {}
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override init() {}
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 }
