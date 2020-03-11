94f67d006e96e05a79e6ff292a4c675ed7fc333c
Luca Gramaglia <lucagramaglia@icloud.com>
2020-03-11 23:47:30 +0100

    Add memory leaks check for components in integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,14 +47,17 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         
         let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
-        
+        let loader = RemoteFeedLoader(url: testServerUrl, client: client)
+
         let exp = expectation(description: "Wait for load completion")
         
-        let loader = RemoteFeedLoader(url: testServerUrl, client: client)
+        trackForMemoryLeaks(client)
+        trackForMemoryLeaks(loader)
+        
         var receivedResult: LoadFeedResult?
         loader.load { result in
             receivedResult = result
